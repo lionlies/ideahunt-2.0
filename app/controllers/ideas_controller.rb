66a6ac7a806cbdb0805a27a3ca129b3aa6f267e2
Jason Lung <lionlies@gmail.com>
2016-07-12 21:05:47 +0800

    added actions and views of ideas#edit, ideas#update, ideas#destroy

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -24,6 +24,25 @@ class IdeasController < ApplicationController
     end
   end
 
+  def edit
+    @idea = Idea.find(params[:id])
+  end
+
+  def update
+    @idea = Idea.find(params[:id])
+    if @idea.update(idea_params)
+      redirect_to ideas_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+    redirect_to ideas_path
+  end
+
   private
 
   def idea_params
